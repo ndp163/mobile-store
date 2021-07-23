79a63d0fbc19d768c1ea8c2d50d5949f2a00c822
phuc <phuc1603>
2021-07-23 12:01:27 +0700

    fix fe

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,2 +1,3 @@
-export const API_URL = process.env.API_URL || "http://localhost:4000";
+export const API_URL =
+    process.env.API_URL || "https://mobilestore308.herokuapp.com";
 export const STORAGE_NAME = process.env.STORAGE_NAME || "c_user";
